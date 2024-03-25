44c81ab3bfce21592f4872cfafb05b9f5b03c10f
Abdlkdrw <A.Nurulla777@yandex.ru>
2024-03-25 23:52:14 +0300

    add HW_2.2

---
+++ Lesson2/Jaeger.java
@@ -0,0 +1,92 @@
+public class Jaeger {
+    
+    private String modelName;
+    private String mark;
+    private String origin;
+    private float height;
+    private float weight;
+    private int speed;
+    private int strength;
+    private int armor;
+
+    public Jaeger(String modelName, String origin, float height, float weight) {
+        this.modelName = modelName;
+        this.origin = origin;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public Jaeger() {
+
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public void goToVictoriaHarbour() {
+        System.out.println("Отправиться в Гавань Виктория для перехвата двух кайдзю, Отачи и Кожистый");
+    }
+
+
+}
---
+++ Lesson2/JaegerTest.java
@@ -0,0 +1,24 @@
+public class JaegerTest {
+    public static void main(String[] args) {
+
+        Jaeger typhoon = new Jaeger();
+        typhoon.setModelName("Crimson Typhoon");
+        typhoon.setMark("Mark-4");
+        typhoon.setOrigin("China");
+        typhoon.setHeight(76.2f);
+        typhoon.setWeight(38.900f);
+        typhoon.setSpeed(9);
+        typhoon.setStrength(8);
+        typhoon.setArmor(6);
+
+        System.out.println("Model name: " + typhoon.getModelName() + "\nOrigin: " + typhoon.getOrigin()
+                           + "\nHeight: " + typhoon.getHeight() + "\nWeight: " + typhoon.getWeight());
+    
+        Jaeger eureka = new Jaeger("Striker Eureka", "Australia", 76.2f, 41.792f);
+
+        System.out.println("Model name: " + eureka.getModelName() + "\nOrigin: " + eureka.getOrigin()
+                           + "\nHeight: " + eureka.getHeight() + "\nWeight: " + eureka.getWeight());
+    
+        typhoon.goToVictoriaHarbour();
+    }
+}
